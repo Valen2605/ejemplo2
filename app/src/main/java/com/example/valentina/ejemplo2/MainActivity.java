74512ade3f3e619591c7c674a7f608ba2ff46401
Valentina <valentinas792@gmail.com>
2017-09-20 20:23:41 -0500

    Initial commit

---
+++ app/src/main/java/com/example/valentina/ejemplo2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.valentina.ejemplo2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    EditText editText;
+    TextView textView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+    }
+
+    public void clickBoton(View vista){
+        textView.setText(editText.getText());
+        Toast.makeText(this, "usted oprimio el boton", Toast.LENGTH_SHORT).show();
+
+    }
+}
